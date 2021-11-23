0f2d9d5deb0a1a7df5208f50bbdc620f336aeac8
puru <94600699+puru-GBM@users.noreply.github.com>
2021-11-23 16:07:53 +0530

    everything ok bro

--- TestDriveKaro/app1/admin.py
+++ TestDriveKaro/app1/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from app1.models import contact
 # Register your models here.
+admin.site.register(contact)
--- TestDriveKaro/app1/models.py
+++ TestDriveKaro/app1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class contact(models.Model):
+    name=models.CharField(max_length=30)
+    email=models.EmailField(max_length=50)
+    companyname=models.CharField(max_length=100)
+    contact=models.IntegerField()
+    message=models.CharField(max_length=300)
