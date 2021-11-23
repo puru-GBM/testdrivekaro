e8030fe3ecf808633f48a59c78f0b00160cdedb6
puru <94600699+puru-GBM@users.noreply.github.com>
2021-11-23 17:43:53 +0530

    contact page done with data base

--- TestDriveKaro/app1/admin.py
+++ TestDriveKaro/app1/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from app1.models import contact
+from app1.models import Contact, filter_top, filter_luxury
 # Register your models here.
-admin.site.register(contact)
+admin.site.register(Contact)
+admin.site.register(filter_top)
+admin.site.register(filter_luxury)
--- TestDriveKaro/app1/models.py
+++ TestDriveKaro/app1/models.py
@@ -1,9 +1,29 @@
 from django.db import models
 
 # Create your models here.
-class contact(models.Model):
+class Contact(models.Model):
     name=models.CharField(max_length=30)
     email=models.EmailField(max_length=50)
-    companyname=models.CharField(max_length=100)
-    contact=models.IntegerField()
+    Subject=models.CharField(max_length=100)
+    contact=models.IntegerField(max_length=20)
     message=models.CharField(max_length=300)
+    
+    def __str__(self):
+        return self.name
+
+
+class filter_top(models.Model):
+    TopBrands=models.CharField(max_length=50)
+    color=models.CharField(max_length=30)
+    Price=models.IntegerField(max_length=20)
+    img=models.ImageField(upload_to="media/img" ,default="")
+    def __str__(self):
+        return self.TopBrands
+
+class filter_luxury(models.Model):
+    Luxury=models.CharField(max_length=50)
+    color=models.CharField(max_length=30)
+    Price=models.IntegerField(max_length=20)
+    img=models.ImageField(upload_to="media/img" ,default="")
+    def __str__(self):
+        return self.Luxury
