402612bcb3ad96cc7c06263888c23c4f5e555869
puru <94600699+puru-GBM@users.noreply.github.com>
2021-11-24 09:47:06 +0530

    some more data added

--- TestDriveKaro/app1/models.py
+++ TestDriveKaro/app1/models.py
@@ -5,7 +5,7 @@ class Contact(models.Model):
     name=models.CharField(max_length=30)
     email=models.EmailField(max_length=50)
     Subject=models.CharField(max_length=100)
-    contact=models.IntegerField(max_length=20)
+    contact=models.IntegerField()
     message=models.CharField(max_length=300)
     
     def __str__(self):
@@ -15,7 +15,7 @@ class Contact(models.Model):
 class filter_top(models.Model):
     TopBrands=models.CharField(max_length=50)
     color=models.CharField(max_length=30)
-    Price=models.IntegerField(max_length=20)
+    Price=models.IntegerField()
     img=models.ImageField(upload_to="media/img" ,default="")
     def __str__(self):
         return self.TopBrands
@@ -23,7 +23,7 @@ class filter_top(models.Model):
 class filter_luxury(models.Model):
     Luxury=models.CharField(max_length=50)
     color=models.CharField(max_length=30)
-    Price=models.IntegerField(max_length=20)
+    Price=models.IntegerField()
     img=models.ImageField(upload_to="media/img" ,default="")
     def __str__(self):
         return self.Luxury
